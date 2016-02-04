3b3564c3f034406198430691348f637ebbfa34bb
pu1s <pu1s@outlook.com>
2016-02-04 15:25:42 +0300

    complete

--- WindowsFormsApplicationTest/Form1.cs
+++ WindowsFormsApplicationTest/Form1.cs
@@ -8,19 +8,44 @@ namespace WindowsFormsApplicationTest
         public Form1()
         {
             InitializeComponent();
-            OAuthBroker broker;
-            Authorization(out broker);
+           
         }
 
-        private static void Authorization(out OAuthBroker broker)
+        private static void Authorization(OAuthBroker broker)
         {
-            broker =
-                new OAuthBroker().RegisterClient(
-                    new OAuthBroker.ConsumerInfo(
-                        "dYhyGEEA4OT6wR4LJn7NC5bhhUP2ISaBxw234CiW",
-                        "N4Gi8a5x8yQ8TuHnKOqPYroeRN63Mmh9k1l5QVxA",
-                        "http://www.bing.com"));
+            
             OAuth.Authorization.RealiseToken(broker);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var cK = string.Empty;
+            var cS = string.Empty;
+            var cU = string.Empty;
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Поле не заполнено...");
+            }
+            else
+                cK = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Поле не заполнено...");
+            }
+            else
+                cS = textBox2.Text;
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Поле не заполнено...");
+            }
+            else
+                cU = textBox3.Text;
+
+            var consumer = new OAuthBroker.ConsumerInfo(cK, cS, cU);
+            var broker = new OAuthBroker().RegisterClient(consumer); 
+            Authorization(broker);
+            textBox4.Text = broker.AccessToken.Token;
+            textBox5.Text = broker.AccessToken.Secret;
         }
     }
 }
